5b1fd389b51d330ec4e9d89dfeef204e2f3ecf92
David <ledavid64@gmail.com>
2018-07-01 21:05:02 -0700

    Add goodDeedsData

--- src/Components/GoodDeeds.js
+++ src/Components/GoodDeeds.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Wrapper from './Wrapper';
 import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
+import { goodDeedsData } from '../config/goodDeedsData';
 
 const Typography = styled.div`
   width: 100%;
@@ -25,18 +26,6 @@ const MyButton = styled(Button)`
   margin: 20px !important;
 `;
 
-const dataGoodDeeds = [
-  { id: 0, deed: 'I gave up my seat on the tube / bus / train' },
-  { id: 1, deed: 'I bought someone a coffee for no reason' },
-  { id: 3, deed: 'I helped an elderly person with his or her shopping bags' },
-  { id: 4, deed: 'I gave a stranger a compliment' },
-  { id: 5, deed: 'I picked up some litter' },
-  { id: 6, deed: 'I gave an old friend a phone call' },
-  { id: 7, deed: 'I donated blood' },
-  { id: 8, deed: 'I called my grandparent(s)' },
-  { id: 9, deed: 'I donated clothes or books to a charity shop' }
-];
-
 export default class extends Component {
   state = {
     gDeeds: ''
@@ -47,7 +36,7 @@ export default class extends Component {
   };
 
   render() {
-    const goodDeeds = dataGoodDeeds.map(e => {
+    const goodDeeds = goodDeedsData.map(e => {
       return (
         <MyButton
           key={e.id}
---
+++ src/config/goodDeedsData.js
@@ -0,0 +1,22 @@
+export const goodDeedsData = [
+  { id: 0, deed: 'I gave up my seat on the tube / bus / train' },
+  { id: 1, deed: 'I bought someone a coffee for no reason' },
+  { id: 3, deed: 'I helped an elderly person with his or her shopping bags' },
+  { id: 4, deed: 'I gave a stranger a compliment' },
+  { id: 5, deed: 'I picked up some litter' },
+  { id: 6, deed: 'I gave an old friend a phone call' },
+  { id: 7, deed: 'I donated blood' },
+  { id: 8, deed: 'I called my grandparent(s)' },
+  { id: 9, deed: 'I donated clothes or books to a charity shop' },
+  { id: 10, deed: 'I introduced myself to my neighbours' },
+  { id: 11, deed: 'I left a waiter or waitress an extra big tip' },
+  { id: 12, deed: 'I helped an elderly or disabled person through the tube' },
+  { id: 13, deed: 'I offered to buy a homeless person something from a shop' },
+  { id: 14, deed: 'I’m giving up meat one day a week' },
+  { id: 15, deed: 'I’ve purchased a recycling bin' },
+  { id: 16, deed: 'I smiled at a stranger' },
+  { id: 17, deed: 'I left a nice note for someone to find on the bus' },
+  { id: 18, deed: 'I asked a stranger who looked upset what was wrong' },
+  { id: 19, deed: 'I sent a nice message to my parent / sibling / relative' },
+  { id: 20, deed: 'I DID SOMETHING ELSE GREAT' }
+];
