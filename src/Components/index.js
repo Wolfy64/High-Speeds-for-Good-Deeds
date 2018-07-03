7e67b5c06f1fbac7c36e8c3e897a9c693c0ad1b7
David <ledavid64@gmail.com>
2018-07-03 14:20:55 -0700

    Fix path in index for Message

--- src/Components/index.js
+++ src/Components/index.js
@@ -6,7 +6,7 @@ export { default as Form } from './Form/Form';
 export { default as GoodDeedList } from './GoodDeedList/GoodDeedList';
 export { default as GoodDeeds } from './GoodDeeds/GoodDeeds';
 export { default as Header } from './Header/Header';
-export { default as Message } from './Board/Messages/Message';
+export { default as Message } from './Layout/Board/Messages/Message';
 export { default as MoneyRaised } from './MoneyRaised/MoneyRaised';
 export { default as Wrapper } from './UI/Wrapper';
 export { default as GoodDeed } from './GoodDeeds/GoodDeed/GoodDeed';
