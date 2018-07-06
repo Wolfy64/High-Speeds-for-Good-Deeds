fcbdafda5b43c693cf4e1d7ba39ef8c7d75d7d50
David <ledavid64@gmail.com>
2018-07-06 16:17:02 -0700

    Move Header and Footer in Layout

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter } from 'react-router-dom';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { App, theme } from './style';
-import { Header, Footer } from '../Components';
 import { Layout } from '../Containers';
 
 export default class extends Component {
@@ -13,9 +12,7 @@ export default class extends Component {
         <App>
           <CssBaseline />
           <MuiThemeProvider theme={theme}>
-            <Header />
             <Layout />
-            <Footer />
           </MuiThemeProvider>
         </App>
       </BrowserRouter>
--- src/Containers/Layout.js
+++ src/Containers/Layout.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { GoodDeed, GoodDeeds, Home, MoneyRaised } from './index';
-import { Error } from '../Components';
+import { Error, Header, Footer } from '../Components';
 
 export default class extends Component {
   state = {};
 
   render() {
     return (
-      <Switch>
-        <Route path="/money-raised" component={MoneyRaised} />
-        <Route path="/good-deeds/:id" component={GoodDeed} />
-        <Route path="/good-deeds" component={GoodDeeds} />
-        <Route path="/" component={Home} exact />
-        <Route component={Error} />
-      </Switch>
+      <React.Fragment>
+        <Header />
+        <Switch>
+          <Route path="/money-raised" component={MoneyRaised} />
+          <Route path="/good-deeds/:id" component={GoodDeed} />
+          <Route path="/good-deeds" component={GoodDeeds} />
+          <Route path="/" component={Home} exact />
+          <Route component={Error} />
+        </Switch>
+        <Footer />
+      </React.Fragment>
     );
   }
 }
