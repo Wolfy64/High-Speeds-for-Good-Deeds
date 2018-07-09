2278d14c57456ff4f6e43351dcb0d69e876ed107
David <ledavid64@gmail.com>
2018-07-09 12:49:15 -0700

    Set up Authentication config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,9 @@
 // Firebase App is always required and must be first
 import firebase from 'firebase';
-import 'firebase';
-import firebaseui from 'firebaseui';
 
 // Add additional services that you want to use
 import 'firebase/database/dist/index.cjs';
 import 'firebase/auth/dist/index.cjs';
-// import 'firebase/firestore';
-// import 'firebase/messaging';
-// import 'firebase/functions';
 
 // Initialize Firebase
 const config = {
@@ -31,35 +26,24 @@ export const dbCounter = database.child('counter');
 // Set authentication
 export const auth = firebase.auth();
 
-const uiConfig = {
+export const uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult: function(authResult, redirectUrl) {
       // User successfully signed in.
       // Return type determines whether we continue the redirect automatically
       // or whether we leave that to developer to handle.
-      console.log('CONNECTED');
-      return true;
+      return false;
     }
   },
-  signInSuccessUrl: '<url-to-redirect-to-on-success>',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     {
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      requireDisplayName: false
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
+      // requireDisplayName: false
     }
-    // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    // firebase.auth.PhoneAuthProvider.PROVIDER_ID
-    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
   ],
   // Terms of service url.
-  tosUrl: '<your-tos-url>'
+  tosUrl: ''
 };
-
-// Initialize the FirebaseUI Widget using Firebase.
-const ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
