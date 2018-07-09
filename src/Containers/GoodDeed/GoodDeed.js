fdbfee57677528867e1ede6642f8dd02280ab146
David <ledavid64@gmail.com>
2018-07-09 15:45:21 -0700

    Add authentication

--- src/Containers/GoodDeed/GoodDeed.js
+++ src/Containers/GoodDeed/GoodDeed.js
@@ -1,22 +1,63 @@
-import React from 'react';
-import { Typography } from './style';
+import React, { Component } from 'react';
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import { Form } from '../../Components';
+import { Typography } from './style';
 
-export default props => {
-  let goodDeed = null;
-  if (props.location.state) {
-    goodDeed = props.location.state.goodDeed;
+export default class extends Component {
+  state = {
+    isConnected: false
+  };
+
+  uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: () => false
+    },
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: ''
+  };
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isConnected: !!user });
+    });
   }
-  return (
-    <React.Fragment>
-      <Typography>
-        {(goodDeed && <p>{goodDeed}</p>) || (
-          <p>Oops! Something went wrong... ¯\_(o_o)_/¯</p>
+
+  render() {
+    let goodDeed = null;
+    if (this.props.location.state) {
+      goodDeed = this.props.location.state.goodDeed;
+    }
+
+    return (
+      <React.Fragment>
+        <Typography>
+          {(goodDeed && <p>{goodDeed}</p>) || (
+            <p>Oops! Something went wrong... ¯\_(o_o)_/¯</p>
+          )}
+        </Typography>
+        {this.state.isConnected ? (
+          <React.Fragment>
+            <Form
+              text={goodDeed || ''}
+              goodDeeds={1}
+              type="Good Deeds"
+              email={firebase.auth().currentUser.email}
+            />
+            <button onClick={() => firebase.auth().signOut()}>Sign Out</button>
+          </React.Fragment>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
         )}
-      </Typography>
-      {props.location && (
-        <Form text={goodDeed || ''} goodDeeds={1} type="Good Deeds" />
-      )}
-    </React.Fragment>
-  );
-};
+      </React.Fragment>
+    );
+  }
+}
