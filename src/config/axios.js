f8747bc1eb5147de742ae5bb7f69d0791f9e8fd8
David <ledavid64@gmail.com>
2018-07-11 14:54:56 -0700

    Add instance justgiving

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: 'https://ironmanon-react.firebaseio.com/'
+const justgivingApi = axios.create({
+  baseURL: 'https://api.justgiving.com/v1/',
+  headers: {
+    'x-api-key': '5b74a57d',
+    accept: 'application/json'
+  }
 });
 
-export default instance;
+export default justgivingApi;
