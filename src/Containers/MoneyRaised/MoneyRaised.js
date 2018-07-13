8a7d281fe6020816bd77da4bcbfbda30d14afa96
David <ledavid64@gmail.com>
2018-07-13 01:44:53 -0700

    Start page MoneyRaised

--- src/Containers/MoneyRaised/MoneyRaised.js
+++ src/Containers/MoneyRaised/MoneyRaised.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Wrapper } from '../../Components';
 
 const MoneyRaised = () => {
-  return <h1>Money Raised</h1>;
+  return (
+    <Wrapper>
+      <h1>Charity 1</h1>
+      <embed
+        type="text/html"
+        src="https://donate.justgiving.com/?uri=aHR0cHM6Ly9kb25hdGUtYXBpLmp1c3RnaXZpbmcuY29tL2FwaS9kb25hdGlvbnMvYjQxY2UzZjc3MDEwNDZjZWExZjc2NGQ3ZjY1Y2ZkNDQ="
+        width="320"
+        height="820"
+      />
+    </Wrapper>
+  );
 };
 
 export default MoneyRaised;
