b9fbc0fd2817f6beda16448470b9d8adf84c65c4
David <ledavid64@gmail.com>
2018-07-14 15:55:44 -0700

    Add Animation Introduction style

--- src/Components/Introduction/Introduction.js
+++ src/Components/Introduction/Introduction.js
@@ -1,198 +1,244 @@
 import React, { Component } from 'react';
 import Tooltip from '@material-ui/core/Tooltip';
+import Collapse from '@material-ui/core/Collapse';
+import Switch from '@material-ui/core/Switch';
+import { withStyles } from '@material-ui/core/styles';
+import { Typography, SwitchWrapper, Wrapper } from './style';
 
-import { Typography, Wrapper } from './style';
+const styles = theme => ({
+  colorSwitchBase: {
+    color: '#026670',
+    '& + $colorBar': {
+      backgroundColor: '#026670'
+    },
+    '&$colorChecked': {
+      color: '#9FEDD7',
+      '& + $colorBar': {
+        backgroundColor: '#9FEDD7'
+      }
+    }
+  },
+  colorBar: {},
+  colorChecked: {}
+});
 
-export default class extends Component {
+class Introduction extends Component {
   state = {
-    collapse: false
+    isOpen: false
   };
 
-  handleChange = () => {
-    this.setState(state => ({ collapse: !state.collapse }));
-  };
+  handleChange = () => this.setState({ isOpen: !this.state.isOpen });
 
   render() {
+    const { classes } = this.props;
     return (
-      <Wrapper onClick={this.handleChange} collapse={this.state.collapse}>
-        <Typography>
-          As some of you may
-          <Tooltip
-            id="tooltip-top-end"
-            title="You probably do know because I’m becoming one of those annoying
-          triathletes who’s life revolves around their upcoming race. Mybad"
-            placement="top-end"
-          >
-            <span> know*</span>
-          </Tooltip>
-          , I am doing an Ironman on July 29th. I am also running the entire
-          coast of Belgium in August in an event I have made up and dubbed the
-          Belgium Border to Border run (BB2B – take note, it’s going to become a
-          thing). This is all very soon - tapering is due to start any minute
-          now.
-        </Typography>
-        <Typography>
-          I have spent a lot of time
-          <Tooltip
-            id="tooltip-top-end"
-            title="There isn’t much else to do during 40km runs"
-            placement="top-end"
-          >
-            <span> thinking** </span>
-          </Tooltip>
-          about which charity I’d like to do these events for, and you will be
-          able to donate to two of those here, but…I also want to try something
-          different!
-        </Typography>
-        <Typography>
-          What I love about Ironman races (and sport in general) is how much it
-          brings complete strangers together. I owe my finish at the 70.3 World
-          <Tooltip
-            id="tooltip-top-end"
-            title="Yep, casual name dropping"
-            placement="top-end"
-          >
-            <span> Champs*** </span>
-          </Tooltip>
-          to a random guy in the crowds who told me I was ‘looking strong!’
-          despite the fact that I was, most certainly, not looking strong at
-          all. (I actually owe that finish to my parents, but let’s pretend it
-          was just that stranger who saved me for the sake of argument.) People
-          will literally stop a race they’ve trained for every single day for
-          the last 12 months if they see someone on the side of the road
-          cramping or in need of help.
-        </Typography>
-        <Typography>
-          In London, commuters will walk past a stranger crying so that they
-          aren’t 30 seconds late to their desk in the morning. We all do it;
-          most of us aren’t even aware that we do it, but all around us there
-          are people struggling and we are busier than an Ironman 11 hours into
-          a race to take a second to stop and ask if we can help.
-        </Typography>
-        <Typography>
-          So here it is; along with raising money for charity, I’m going to try
-          something a bit different...
-        </Typography>
-        <Typography>
-          A few months ago, a very close friend of mine gave
-          <Tooltip
-            id="tooltip-top-end"
-            title="I actually stole it. Sorry Chris"
-            placement="top-end"
-          >
-            <span> me**** </span>
-          </Tooltip>
-          a black wrist band with two words on it: Compassion it. It’s aim is to
-          encourage the wearer, i.e. me, to do something compassionate that day.
-          Once I’ve done it, I can flip the wrist band over to the white side.
-          Go to sleep feeling that little bit happier. Flip the wristband back
-          to the black side. Repeat.
-        </Typography>
-        <Typography>
-          After a couple of weeks of wearing it, I didn’t even need the reminder
-          of the wristband – I had become so much more observant of the world
-          around me and was actively looking for how I could help strangers.
-          This all sounds a little intense, but it was just the slightest change
-          in my perception of the world, and it made a world of a difference.
-        </Typography>
-        <Typography>
-          Anyway, blah blah – I’m babbling. Here’s the plan:
-        </Typography>
-        <Typography>
-          If you’d like to sponsor me – wait, first let me remind you what I am
-          actually going to be doing:
-        </Typography>
-        <Typography bold>
-          I’m going to willingly wake up at 4am on a summer’s day to swim 3.8km,
-          cycle 180km around Whistler (Whistler is very hilly), and then run a
-          marathon. Back to back. Nonstop.
-        </Typography>
-        <Typography>
-          Then I’m going to sleep lots and do some adventuring and a few weeks
-          later I am going to willingly wake up at 4am again, watch the sunrise
-          in France (everyone cross their fingers for a nice sunrise), run
-          approximately 75km of beautiful, stunning, Belgian coastline, and
-          hopefully make it to Holland for sunset.
-        </Typography>
-        <Typography>
-          Ok, so, with all that in mind if you’d like to sponsor me, here’s what
-          I would love for you to do:
-        </Typography>
-        <Typography>
-          <b>OPTION 1:</b> Do something nice for someone else today. Buy a
-          coffee for one of your colleagues that’s looking a little down.
-          Actively look up in the tube to see if there is someone you can offer
-          your seat to. Ask a homeless person sitting outside of Tesco’s if you
-          can buy them anything from inside. Pick up some trash from the street.
-          Help an old lady onto the bus.
-        </Typography>
-        <Typography>
-          GUYS THERE ARE SO MANY NICE THINGS YOU CAN BE DOING.
-        </Typography>
-        <Typography>
-          <b>OPTION 2:</b> You can also donate to charity. I’ve chosen two
-          charities here that mean so much to me:
-        </Typography>
-        <Typography>
-          Make-A-Wish UK helps grant children with terminal illnesses a wish.
-          Your donation will help 5-year-old Steven become a policeman for the
-          day, or help Alice meet her idol. It is an incredible charity that
-          brightens up these children’s lives.{' '}
-        </Typography>
-        <Typography>
-          Age UK focusses on improving the life of the elderly by helping them
-          be more independent, organising events for them, and generally looking
-          after those who are finding later life difficult. Their work is
-          heartwarming.
-        </Typography>
-        <hr />
-        <Typography>
-          I hate reading the news nowadays because it’s always sad and it makes
-          me cry (yeah guys I cry a lot), and literally the greatest things to
-          read are those posts called ‘A reminder that humanity still exists’.{' '}
-        </Typography>
-        <Typography bold>
-          I want this page to be my very own “Humanity still exists” post.
-        </Typography>
-        <Typography>
-          If you want to sponsor me (if you still need convincing please reread
-          previous section of this essay), pick one of the options and do it
-          today. COME ON. NO CHEATING. It’s going to be fun, I promise.{' '}
-        </Typography>
-        <Typography>
-          Post it on here to show off that you’re a great human being and to
-          motivate me to keep training (also so that I feel like I have lots of
-          friends). This page will keep a tally of all good deeds & of all
-          donations.{' '}
-        </Typography>
-        <Typography>
-          There are 16 days between now and the Ironman (oh wow that’s not a
-          lot) and 43 days until the Belgian Ultra, so if you wanted to, you
-          could do 43 good deeds and make me feel uber popular by posting them
-          all on here. Also imagine how much greater so many strangers' lives
-          will be.
-        </Typography>
-        <Typography>
-          GUYS THIS IS ONLY GOING TO WORK IF PEOPLE ACTUALLY DO THINGS - GO BE
-          GREAT.
-        </Typography>
-        <Typography>
-          WAIT DON’T GO YET. LAST BUT VERY DEFINITELY NOT LEAST - I want to
-          thank David De Wulf for being an absolute superhero and creating this
-          website for me in record time. You are a legend and I cannot thank you
-          enough!
-        </Typography>
-        <Typography>xxxxxxx</Typography>
-        <Typography smaller>
-          *You probably do know because I’m becoming one of those annoying
-          triathletes who’s life revolves around their upcoming race. Mybad.
-        </Typography>
-        <Typography smaller>
-          **There isn’t much else to do during 40km runs
-        </Typography>
-        <Typography smaller>*** Yep, casual name dropping.</Typography>
-        <Typography smaller>****I actually stole it. Sorry Chris</Typography>
+      <Wrapper>
+        {/* <Switch checked={this.state.isOpen} onChange={this.handleChange} /> */}
+        <Collapse
+          in={this.state.isOpen}
+          collapsedHeight="300px"
+          onClick={this.handleChange}
+        >
+          <Typography>
+            As some of you may
+            <Tooltip
+              id="tooltip-top-end"
+              title="You probably do know because I’m becoming one of those annoying
+                triathletes who’s life revolves around their upcoming race. Mybad"
+              placement="top-end"
+            >
+              <span> know*</span>
+            </Tooltip>
+            , I am doing an Ironman on July 29th. I am also running the entire
+            coast of Belgium in August in an event I have made up and dubbed the
+            Belgium Border to Border run (BB2B – take note, it’s going to become
+            a thing). This is all very soon - tapering is due to start any
+            minute now.
+          </Typography>
+          <Typography>
+            I have spent a lot of time
+            <Tooltip
+              id="tooltip-top-end"
+              title="There isn’t much else to do during 40km runs"
+              placement="top-end"
+            >
+              <span> thinking** </span>
+            </Tooltip>
+            about which charity I’d like to do these events for, and you will be
+            able to donate to two of those here, but…I also want to try
+            something different!
+          </Typography>
+          <Typography>
+            What I love about Ironman races (and sport in general) is how much
+            it brings complete strangers together. I owe my finish at the 70.3
+            World
+            <Tooltip
+              id="tooltip-top-end"
+              title="Yep, casual name dropping"
+              placement="top-end"
+            >
+              <span> Champs*** </span>
+            </Tooltip>
+            to a random guy in the crowds who told me I was ‘looking strong!’
+            despite the fact that I was, most certainly, not looking strong at
+            all. (I actually owe that finish to my parents, but let’s pretend it
+            was just that stranger who saved me for the sake of argument.)
+            People will literally stop a race they’ve trained for every single
+            day for the last 12 months if they see someone on the side of the
+            road cramping or in need of help.
+          </Typography>
+          <Typography>
+            In London, commuters will walk past a stranger crying so that they
+            aren’t 30 seconds late to their desk in the morning. We all do it;
+            most of us aren’t even aware that we do it, but all around us there
+            are people struggling and we are busier than an Ironman 11 hours
+            into a race to take a second to stop and ask if we can help.
+          </Typography>
+          <Typography>
+            So here it is; along with raising money for charity, I’m going to
+            try something a bit different...
+          </Typography>
+          <Typography>
+            A few months ago, a very close friend of mine gave
+            <Tooltip
+              id="tooltip-top-end"
+              title="I actually stole it. Sorry Chris"
+              placement="top-end"
+            >
+              <span> me**** </span>
+            </Tooltip>
+            a black wrist band with two words on it: Compassion it. It’s aim is
+            to encourage the wearer, i.e. me, to do something compassionate that
+            day. Once I’ve done it, I can flip the wrist band over to the white
+            side. Go to sleep feeling that little bit happier. Flip the
+            wristband back to the black side. Repeat.
+          </Typography>
+          <Typography>
+            After a couple of weeks of wearing it, I didn’t even need the
+            reminder of the wristband – I had become so much more observant of
+            the world around me and was actively looking for how I could help
+            strangers. This all sounds a little intense, but it was just the
+            slightest change in my perception of the world, and it made a world
+            of a difference.
+          </Typography>
+          <Typography>
+            Anyway, blah blah – I’m babbling. Here’s the plan:
+          </Typography>
+          <Typography>
+            If you’d like to sponsor me – wait, first let me remind you what I
+            am actually going to be doing:
+          </Typography>
+          <Typography bold>
+            I’m going to willingly wake up at 4am on a summer’s day to swim
+            3.8km, cycle 180km around Whistler (Whistler is very hilly), and
+            then run a marathon. Back to back. Nonstop.
+          </Typography>
+          <Typography>
+            Then I’m going to sleep lots and do some adventuring and a few weeks
+            later I am going to willingly wake up at 4am again, watch the
+            sunrise in France (everyone cross their fingers for a nice sunrise),
+            run approximately 75km of beautiful, stunning, Belgian coastline,
+            and hopefully make it to Holland for sunset.
+          </Typography>
+          <Typography>
+            Ok, so, with all that in mind if you’d like to sponsor me, here’s
+            what I would love for you to do:
+          </Typography>
+          <Typography>
+            <b>OPTION 1:</b> Do something nice for someone else today. Buy a
+            coffee for one of your colleagues that’s looking a little down.
+            Actively look up in the tube to see if there is someone you can
+            offer your seat to. Ask a homeless person sitting outside of Tesco’s
+            if you can buy them anything from inside. Pick up some trash from
+            the street. Help an old lady onto the bus.
+          </Typography>
+          <Typography>
+            GUYS THERE ARE SO MANY NICE THINGS YOU CAN BE DOING.
+          </Typography>
+          <Typography>
+            <b>OPTION 2:</b> You can also donate to charity. I’ve chosen two
+            charities here that mean so much to me:
+          </Typography>
+          <Typography>
+            Make-A-Wish UK helps grant children with terminal illnesses a wish.
+            Your donation will help 5-year-old Steven become a policeman for the
+            day, or help Alice meet her idol. It is an incredible charity that
+            brightens up these children’s lives.{' '}
+          </Typography>
+          <Typography>
+            Age UK focusses on improving the life of the elderly by helping them
+            be more independent, organising events for them, and generally
+            looking after those who are finding later life difficult. Their work
+            is heartwarming.
+          </Typography>
+          <hr />
+          <Typography>
+            I hate reading the news nowadays because it’s always sad and it
+            makes me cry (yeah guys I cry a lot), and literally the greatest
+            things to read are those posts called ‘A reminder that humanity
+            still exists’.{' '}
+          </Typography>
+          <Typography bold>
+            I want this page to be my very own “Humanity still exists” post.
+          </Typography>
+          <Typography>
+            If you want to sponsor me (if you still need convincing please
+            reread previous section of this essay), pick one of the options and
+            do it today. COME ON. NO CHEATING. It’s going to be fun, I promise.{' '}
+          </Typography>
+          <Typography>
+            Post it on here to show off that you’re a great human being and to
+            motivate me to keep training (also so that I feel like I have lots
+            of friends). This page will keep a tally of all good deeds & of all
+            donations.{' '}
+          </Typography>
+          <Typography>
+            There are 16 days between now and the Ironman (oh wow that’s not a
+            lot) and 43 days until the Belgian Ultra, so if you wanted to, you
+            could do 43 good deeds and make me feel uber popular by posting them
+            all on here. Also imagine how much greater so many strangers' lives
+            will be.
+          </Typography>
+          <Typography>
+            GUYS THIS IS ONLY GOING TO WORK IF PEOPLE ACTUALLY DO THINGS - GO BE
+            GREAT.
+          </Typography>
+          <Typography>
+            WAIT DON’T GO YET. LAST BUT VERY DEFINITELY NOT LEAST - I want to
+            thank David De Wulf for being an absolute superhero and creating
+            this website for me in record time. You are a legend and I cannot
+            thank you enough!
+          </Typography>
+          <Typography>xxxxxxx</Typography>
+          <Typography smaller italic>
+            *You probably do know because I’m becoming one of those annoying
+            triathletes who’s life revolves around their upcoming race. Mybad.
+          </Typography>
+          <Typography smaller italic>
+            **There isn’t much else to do during 40km runs
+          </Typography>
+          <Typography smaller italic>
+            *** Yep, casual name dropping.
+          </Typography>
+          <Typography smaller italic>
+            ****I actually stole it. Sorry Chris
+          </Typography>
+        </Collapse>
+        <SwitchWrapper isOpen={this.state.isOpen}>
+          {this.state.isOpen ? 'Close it' : 'Read more'}
+          <Switch
+            checked={this.state.isOpen}
+            onChange={this.handleChange}
+            classes={{
+              switchBase: classes.colorSwitchBase,
+              checked: classes.colorChecked,
+              bar: classes.colorBar
+            }}
+          />
+        </SwitchWrapper>
       </Wrapper>
     );
   }
 }
+
+export default withStyles(styles)(Introduction);
