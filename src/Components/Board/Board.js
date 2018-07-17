e608347c5b44ef360e07507f1dd380835d55f25d
David <ledavid64@gmail.com>
2018-07-17 02:44:28 -0700

    Add style

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -59,17 +59,20 @@ export default class MessageBoard extends Component {
   handleToggleModal = () => this.setState({ showModal: !this.state.showModal });
 
   handleShowImage = event => {
-    console.log(event.target);
     const image = {
       src: event.target.src,
       title: event.target.alt
     };
     this.handleToggleModal();
     this.setState({ image });
-    console.log(this.state.image);
   };
 
   componentDidMount() {
+    window.addEventListener(
+      'resize',
+      () => this.setState({ showCarousel: window.innerWidth < 700 }),
+      false
+    );
     this.setState({ images: [...images] });
     this.getGoodDeedsMessages();
     this.getMoneyRaisedMessages();
@@ -77,22 +80,40 @@ export default class MessageBoard extends Component {
   }
 
   render() {
-    window.addEventListener(
-      'resize',
-      () => this.setState({ showCarousel: window.innerWidth < 700 }),
-      false
-    );
-    console.log(this.state.messages.length > 1);
     return (
       <Background>
         <Modal show={this.state.showModal} onClick={this.handleToggleModal}>
-          <img
-            width="50%"
-            height="auto"
-            src={this.state.image.src}
-            alt={this.state.image.title}
-          />
-          <p>{this.state.image.title}</p>
+          <div>
+            <img
+              style={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                width: '500px',
+                height: '500px',
+                marginTop: '-250px',
+                marginLeft: '-250px'
+              }}
+              width="50%"
+              height="auto"
+              src={this.state.image.src}
+              alt={this.state.image.title}
+            />
+            <h2
+              style={{
+                color: 'white',
+                position: 'absolute',
+                width: '100%',
+                bottom: '10%',
+                background: 'rgba(0, 0, 0, 0.5)',
+                width: '50%',
+                left: '25%',
+                borderRadius: '5px'
+              }}
+            >
+              {this.state.image.title}
+            </h2>
+          </div>
         </Modal>
         <Wrapper>
           <Container>
