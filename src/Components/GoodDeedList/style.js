6ef2c2b2581e4e9a3e849c67fcbdecfee74387ce
David <ledavid64@gmail.com>
2018-07-17 09:57:10 -0700

    Changed css margin

--- src/Components/GoodDeedList/style.js
+++ src/Components/GoodDeedList/style.js
@@ -2,8 +2,14 @@ import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
 
 export const MyButton = styled(Button)`
-  min-width: 200px !important;
-  width: 300px;
-  height: 50px;
-  margin: 20px !important;
+  && {
+    min-width: 200px;
+    width: 300px;
+    height: 50px;
+    margin: 20px;
+
+    @media (max-width: 600px) {
+      margin: 20px 0px;
+    }
+  }
 `;
