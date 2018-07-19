aacdf08ab9ef50104a7dc4521edd57920a5432ff
David <ledavid64@gmail.com>
2018-07-19 11:03:41 -0700

    Change Caps text to lowerCase

--- src/Components/GoodDeedList/style.js
+++ src/Components/GoodDeedList/style.js
@@ -3,6 +3,7 @@ import Button from '@material-ui/core/Button';
 
 export const MyButton = styled(Button)`
   && {
+    text-transform: initial;
     min-width: 200px;
     width: 300px;
     height: 50px;
