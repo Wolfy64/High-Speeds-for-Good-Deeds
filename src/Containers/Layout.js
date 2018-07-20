65a5eabf1d7d54a509387ad0dc99c13ea75a6ccf
David <ledavid64@gmail.com>
2018-07-19 23:30:59 -0700

    Change to stateless & Add style

--- src/Containers/Layout.js
+++ src/Containers/Layout.js
@@ -1,32 +1,21 @@
 import React, { Component } from 'react';
+import { Wrapper } from './style';
 import { Route, Switch } from 'react-router-dom';
 import { GoodDeed, GoodDeeds, Home, MoneyRaised } from './index';
-import { Error, Header, Footer, Wrapper } from '../Components';
+import { Error, Header, Footer } from '../Components';
 
-export default class extends Component {
-  state = {};
-
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <Wrapper
-          style={{
-            minHeight: '97vh',
-            paddingTop: '150px',
-            textAlign: 'center'
-          }}
-        >
-          <Switch>
-            <Route path="/money-raised" component={MoneyRaised} />
-            <Route path="/good-deeds/:id" component={GoodDeed} />
-            <Route path="/good-deeds" component={GoodDeeds} />
-            <Route path="/" component={Home} exact />
-            <Route component={Error} />
-          </Switch>
-        </Wrapper>
-        <Footer />
-      </React.Fragment>
-    );
-  }
-}
+export default () => (
+  <React.Fragment>
+    <Header />
+    <Wrapper>
+      <Switch>
+        <Route path="/money-raised" component={MoneyRaised} />
+        <Route path="/good-deeds/:id" component={GoodDeed} />
+        <Route path="/good-deeds" component={GoodDeeds} />
+        <Route path="/" component={Home} exact />
+        <Route component={Error} />
+      </Switch>
+    </Wrapper>
+    <Footer />
+  </React.Fragment>
+);
---
+++ src/Containers/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  margin: auto;
+  max-width: 960px;
+  text-align: center;
+  min-height: 88vh;
+  padding-top: 150px;
+  text-align: center;
+`;
