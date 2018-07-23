a6b3930ceb849b459df0632da93045d2c27ed4f4
David <ledavid64@gmail.com>
2018-07-23 09:39:00 -0700

    Add feature pagination

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import { justgivingApi, firebaseApi, images } from '../../config';
-import { Background, Container } from './style';
+import {
+  Background,
+  MyButton,
+  Container,
+  ContainerBtn,
+  ContainerMsg
+} from './style';
+import next from './../../assets/next.svg';
+import previous from './../../assets/previous.svg';
 import { Messages, Modal, Images, ImageZoomed, Wrapper } from '../index';
 
 export default class MessageBoard extends Component {
@@ -9,13 +17,23 @@ export default class MessageBoard extends Component {
     images: [],
     showCarousel: false,
     showModal: false,
-    image: {}
+    image: {},
+    currentMessages: [],
+    currentPage: 1,
+    maxPage: 0,
+    messagesPerPage: 10
   };
 
   componentDidMount() {
     this.getScreenSize();
     this.getImages();
     this.getMessages();
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.currentPage !== this.state.currentPage) {
+      this.doPagination();
+    }
   }
 
   getScreenSize() {
@@ -80,6 +98,7 @@ export default class MessageBoard extends Component {
       // Sort message by date
       messages.sort((a, b) => b.date - a.date);
       this.setState({ messages });
+      this.doPagination();
     });
   }
 
@@ -90,6 +109,31 @@ export default class MessageBoard extends Component {
     this.handleToggleModal();
     this.setState({ image });
   };
+
+  handleNextPage = () => {
+    if (this.state.currentPage < this.state.maxPage) {
+      this.setState(prevState => ({
+        currentPage: prevState.currentPage + 1
+      }));
+    }
+  };
+
+  handlePreviousPage = () => {
+    if (this.state.currentPage > 1) {
+      this.setState(prevState => ({
+        currentPage: prevState.currentPage - 1
+      }));
+    }
+  };
+
+  doPagination() {
+    const { messages, currentPage, messagesPerPage } = this.state;
+    const indexLastMessage = currentPage * messagesPerPage;
+    const indexFirstMessage = indexLastMessage - messagesPerPage;
+    const currentMessages = messages.slice(indexFirstMessage, indexLastMessage);
+    const maxPage = Math.ceil(messages.length / messagesPerPage);
+    this.setState({ currentMessages, maxPage });
+  }
 
   render() {
     return (
@@ -106,7 +150,27 @@ export default class MessageBoard extends Component {
               images={this.state.images}
               clicked={this.handleShowImage}
             />
-            <Messages messages={this.state.messages} />
+            <ContainerMsg>
+              <Messages messages={this.state.currentMessages} />
+              <ContainerBtn>
+                <MyButton
+                  variant="contained"
+                  color="secondary"
+                  onClick={this.handlePreviousPage}
+                  disabled={this.state.currentPage === 1}
+                >
+                  <img src={previous} alt="previous" />
+                </MyButton>
+                <MyButton
+                  variant="contained"
+                  color="secondary"
+                  onClick={this.handleNextPage}
+                  disabled={this.state.currentPage === this.state.maxPage}
+                >
+                  <img src={next} alt="next" />
+                </MyButton>
+              </ContainerBtn>
+            </ContainerMsg>
           </Container>
         </Wrapper>
       </Background>
--- src/Components/Board/style.js
+++ src/Components/Board/style.js
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
 
 export const Background = styled.div`
   background-color: #fef9c7;
   border-radius: 5px;
+`;
+
+export const MyButton = styled(Button)`
+  && {
+    height: 50px;
+    margin-bottom: 20px;
+  }
 `;
 
 export const Container = styled.div`
@@ -12,4 +20,14 @@ export const Container = styled.div`
   @media (max-width: 700px) {
     flex-direction: column;
   }
+`;
+
+export const ContainerBtn = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin: 10px 0;
+`;
+
+export const ContainerMsg = styled.div`
+  flex: 2;
 `;
